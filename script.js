1985c59b234feaaae40092018979ba4f868a35d3
wasimkhushnuma-cmyk <wasimkhushnuma@gmail.com>
2026-06-21 19:42:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
-console.log(
-"Heart of the Family loaded"
+function addMemory(){
+
+const div=
+document.createElement(
+"div"
 )
+
+div.className=
+"memory"
+
+div.innerHTML=`
+
+<input
+placeholder=
+"Photo URL">
+
+<textarea
+placeholder=
+"Message">
+
+</textarea>
+
+`
+
+document
+.getElementById(
+"memories"
+)
+.appendChild(
+div
+)
+
+}
+
+function generate(){
+
+const id=
+
+Math.random()
+
+.toString(36)
+
+.slice(2)
+
+window.location=
+
+`../view.html?id=${id}`
+
+}
